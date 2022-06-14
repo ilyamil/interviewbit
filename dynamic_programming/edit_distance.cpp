0dc476c1adc523772a88569e03d7e6115411e785
imilyutin <idmilyutin@gmail.com>
2022-06-15 00:11:03 +0300

    added solution to problem Edit Distance

---
+++ dynamic_programming/edit_distance.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+int find_edit_distance(std::string A, std::string B) {
+    int m = A.length();
+    int n = B.length();
+    std::vector<std::vector<int>> dp(m + 1, std::vector<int>(n + 1));
+
+    for (int i = 0; i <= m; i++) {
+        dp[i][0] = i;
+    }
+    for (int j = 0; j <= n; j++) {
+        dp[0][j] = j;
+    }
+
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (A[i - 1] == B[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            }
+            else {
+                dp[i][j] = std::min(dp[i - 1][j - 1], std::min(dp[i - 1][j], dp[i][j - 1])) + 1;
+            }
+        }
+    }
+
+    return dp[m][n];
+}
+
+int main() {
+    std::string A = "abac";
+    std::string B = "abad";
+    std::cout << find_edit_distance(A, B);
+}
